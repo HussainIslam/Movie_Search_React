880014557d7fd7c9baf7c7bcfb984cc8683f5fe7
hussain <m.hussainul.islam@gmail.com>
2019-06-22 22:04:42 -0400

    Movie component under implementation

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import getData from '../fetchData'
 
 export default class extends React.Component{
-  
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: false,
+            errored: false,
+            search: false,
+            searchString: null,
+            currentPage: 1,
+            movieList: null
+        }
+    }
+
+    async componentDidMount(){
+        try{
+            this.setState({laoding: true});
+            const resepone = await getData()
+        }
+        catch(error){
+
+        }
+
+    }
+
 }
