f53e6a7854bc01d4e679a6d01b7bb0e95d3a82b4
hussain <m.hussainul.islam@gmail.com>
2019-06-23 00:37:44 -0400

    structure of Movies done

---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default ()=>{
+    return(
+        <div>
+            <p>Movie image</p>
+            <p>Movie name</p>
+        </div>
+
+    );
+}
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import getData from '../fetchData'
 
 export default class extends React.Component{
@@ -18,12 +17,39 @@ export default class extends React.Component{
     async componentDidMount(){
         try{
             this.setState({laoding: true});
-            const resepone = await getData()
+            const response = await getData();
+            console.log(response);
+
         }
         catch(error){
 
         }
 
+    }
+
+    render(){
+        if(this.state.errored){
+            return(
+                <div>
+                    <p>Error: unable to load data</p>
+                </div>
+            );
+        }
+
+        if(this.state.loading){
+            return(
+                <div>
+                    <p>Loading the data ...</p>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <p>These are the movies</p>
+            </div>
+        );
+        
     }
 
 }
